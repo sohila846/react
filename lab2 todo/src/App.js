2c33c687b965cab72a181f5fc0e048a5f79b19db
sohila <msohila429@gmail.com>
2021-10-28 11:29:27 +0200

    lab 2

---
+++ lab2 todo/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ToDo App</h1>
+     
+       <TodoList/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ lab2 todo/src/components/TodoList.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = todo => {
+    
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
+
+  const removeTodo = id => {
+    const removedArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removedArr);
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
